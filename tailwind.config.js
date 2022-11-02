e77d99e8a41237180763113be59038b0ea3ea621
yuesth <yues.963@gmail.com>
2022-11-02 15:45:20 +0700

    chore: init tailwindcss, eslint, prettier, font

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: `#FF731C`,
+				base: '#F5F5FB',
+				textDark: '#393939',
+				textLight: `#969BAB`,
+				border: `#C4C4C6`
+			},
+		},
+		fontFamily: {
+			sans: ['Poppins', 'sans-serif'],
+		}
+	},
+	plugins: [],
+}
