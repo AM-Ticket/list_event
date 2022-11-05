b0fe1d84fcda66c8c63ba6aab5fbd4fd3791ae8f
irfi <irfi@localcitizen>
2022-11-05 20:25:16 +0700

    feat: integrate my tickets

--- pages/my-tickets.tsx
+++ pages/my-tickets.tsx
@@ -1,12 +1,67 @@
-import { useState } from 'react'
-import Button from '../components/Button'
+// @ts-nocheck
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import Nav from '../components/Nav'
 import NavbarTop from '../components/NavbarTop'
-import NFTImage from '../components/NFTImage'
-import QRModal from '../components/QRModal'
+import TicketModal from '../components/TicketModal'
+import { useNear } from '../contexts/near'
 
 const MyTickets = () => {
-	const [showQR, setShowQR] = useState<boolean>(false)
+	const [eventList, setEventList] = useState(null)
+	const [ownedTicketList, setOwnedTicketList] = useState(null)
+	const { wallet } = useNear()
+	const router = useRouter()
+
+	useEffect(() => {
+		const fetchEvents = async () => {
+			const events = await axios.get('/api/events')
+			setEventList(events.data.data)
+		}
+
+		fetchEvents().catch(console.error)
+	}, [])
+
+	useEffect(() => {
+		const account_id = wallet?.getAccountId()
+		if (eventList && eventList.length > 0) {
+			const contracts = eventList
+				.filter((event) => event.subaccount)
+				.map((event) => {
+					return event.subaccount
+				})
+
+			let nftsOwned = []
+			contracts.map(async (contract) => {
+				let nftsOwnedPerContract = []
+				const supply = await wallet?.account().viewFunction({
+					contractId: contract,
+					methodName: 'nft_supply_for_owner',
+					args: {
+						account_id,
+					},
+				})
+				let nftsResult = [0]
+				let i = 0
+				while (nftsOwnedPerContract.length < supply) {
+					nftsResult = await wallet?.account().viewFunction({
+						contractId: contract,
+						methodName: 'nft_tokens_for_owner',
+						args: {
+							account_id,
+							from_index: i.toString(),
+							limit: 10,
+						},
+					})
+					i += 10
+					nftsOwnedPerContract = [...nftsResult, ...nftsOwnedPerContract]
+				}
+				nftsOwned = { [contract]: nftsOwnedPerContract, ...nftsOwned }
+				setOwnedTicketList(nftsOwned)
+			})
+		}
+	}, [eventList])
+
 	return (
 		<div className="max-w-[2560px] w-full bg-base min-h-screen flex">
 			<Nav />
@@ -18,78 +73,21 @@ const MyTickets = () => {
 						My Tickets
 					</p>
 					<div className="grid grid-cols-2 gap-6">
-						<div className="flex items-center justify-center">
-							<div className="flex space-x-8 rounded-xl shadow-xl p-6">
-								<NFTImage size="base" />
-								<div className="flex flex-col justify-between">
-									<div>
-										<p className="font-extrabold text-xl text-textDark mb-4">
-											Ticket #1
-										</p>
-										<p className="text-sm text-textDark">View Details</p>
-									</div>
-									<div>
-										<Button
-											color="black"
-											size="base"
-											onClickHandler={() => setShowQR(true)}
+						{ownedTicketList &&
+							Object.keys(ownedTicketList).map((contractId) => {
+								return ownedTicketList[contractId].map((nft) => {
+									return (
+										<TicketModal
+											url={`${process.env.NEXT_PUBLIC_DOMAIN}/redeem?token_id=${nft.token_id}&contract_id=${contractId}`}
 										>
-											View QR COde
-										</Button>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div className="flex items-center justify-center">
-							<div className="flex space-x-8 rounded-xl shadow-xl p-6">
-								<NFTImage size="base" />
-								<div className="flex flex-col justify-between">
-									<div>
-										<p className="font-extrabold text-xl text-textDark mb-4">
-											Ticket #2
-										</p>
-										<p className="text-sm text-textDark">View Details</p>
-									</div>
-									<div>
-										<Button
-											color="black"
-											size="base"
-											onClickHandler={() => setShowQR(true)}
-										>
-											View QR COde
-										</Button>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div className="flex items-center justify-center">
-							<div className="flex space-x-8 rounded-xl shadow-xl p-6">
-								<NFTImage size="base" />
-								<div className="flex flex-col justify-between">
-									<div>
-										<p className="font-extrabold text-xl text-textDark mb-4">
-											Ticket #3
-										</p>
-										<p className="text-sm text-textDark">View Details</p>
-									</div>
-									<div>
-										<Button
-											color="black"
-											size="base"
-											onClickHandler={() => setShowQR(true)}
-										>
-											View QR COde
-										</Button>
-									</div>
-								</div>
-							</div>
-						</div>
+											{nft.metadata.title}
+										</TicketModal>
+									)
+								})
+							})}
 					</div>
 				</div>
 			</div>
-			<QRModal isShow={showQR} onClose={() => setShowQR(false)} />
 		</div>
 	)
 }
